d3e466491589702e3b84495b8a065f91d91c3a4c
developersbestoption <139047026+developersbestoption@users.noreply.github.com>
2026-08-02 11:56:28 +0530

    palindrom program

--- day3.py
+++ day3.py
@@ -1,1 +1,17 @@
-print("hello")
+#checking wether given number is palindrom or not
+number=int(input("enter the number :"))
+number1=number
+new_number=0
+while True:
+    last_digit=number%10
+    new_number+=last_digit
+    number//=10
+    if number==0:
+                break
+    new_number*=10
+
+print(f" new number {new_number}")
+if new_number==number1:
+        print(f"{number1} is palindrom number")
+else:
+        print(f"{number1} is not a palindrom number")
