728782ac21353bcf559cd796b95fb1709f921331
developersbestoption <139047026+developersbestoption@users.noreply.github.com>
2026-08-04 01:23:23 +0530

    swap using temp

---
+++ day4/swap_using_temp.py
@@ -0,0 +1,11 @@
+a=int(input("enter the number : "))
+b=int(input("enter the number : "))
+print(f"a : {a}")
+print(f"b : {b}")
+temp =None
+temp=a
+a=b
+b=temp
+print(f"a : {a}")
+print(f"b : {b}")
+
