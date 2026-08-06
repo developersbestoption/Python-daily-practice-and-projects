df59eff89acd88199a5f7590f4c71e01556f8cd6
developersbestoption <139047026+developersbestoption@users.noreply.github.com>
2026-08-06 23:14:51 +0530

    swaping the numbers without temp avriable

--- day_5/String.py
+++ day_5/String.py
@@ -1,4 +1,3 @@
 #reversing a string by using the indexing
 string=input("enter your string :")
 print(string[::-1])
-
---
+++ day_5/swaping_without_temp.py
@@ -0,0 +1,11 @@
+a=int(input("enter first number : "))
+b=int(input("enter second number : "))
+print("before swaping")
+print("a: ",a)
+print("b :",b)
+a=a+b
+b=a-b 
+a=a-b
+print("after swaping")
+print("a :",a)
+print("b :",b)
