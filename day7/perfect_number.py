8f6f9f36a549c32b446a618eec11540b1b2bb3b2
developersbestoption <139047026+developersbestoption@users.noreply.github.com>
2026-08-07 21:09:08 +0530

    perfect number

---
+++ day7/perfect_number.py
@@ -0,0 +1,12 @@
+number=int(input("enter the number :"))
+if number<=0:
+    print("not a perfect number")
+else:
+    new=0
+    for i in range(1,number):
+        if number%i==0:
+            new+=i 
+    if new == number:
+         print("perfect number")
+    else:
+         print("not a perfect number")'''
