e1ce8fd50aaa9d20f76e651bf79d254d42362bfb
developersbestoption <139047026+developersbestoption@users.noreply.github.com>
2026-08-08 23:46:18 +0530

    sentence reversing

--- day8/sentence_reverse.py
+++ day8/sentence_reverse.py
@@ -1,4 +1,4 @@
 a=input("enter the sentence :")
 words=a.split()#this splits the sentence and make list of words
 for i in words:#this takes each word into a i and we perform the revesing by indexing 
-    print(i[::-1],end=" ")# we use "end" to continue in the same line
+    print(i[::-1],end=" ")# we use "end" to continue in the same  line
