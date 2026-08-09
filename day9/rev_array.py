422f518daa597566d5c2389b0d67ab0259bf8bf5
developersbestoption <139047026+developersbestoption@users.noreply.github.com>
2026-08-09 23:18:48 +0530

    reversing array

---
+++ day9/rev_array.py
@@ -0,0 +1,8 @@
+a=list(map(int,input("enter array elements : ").split()))
+print(a)
+a.reverse()
+print(a)
+'''
+1.sort(reverse method sort the array in descending order not reverse the array)
+2. we can use another method to reverse a array slicing [::-1] and making new array and adding the elenments in reverse order by negitive indexing
+'''
