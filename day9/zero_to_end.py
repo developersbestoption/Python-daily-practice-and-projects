ae6564f49749982bc9c6a1ac9e6ae7f2be1e271b
developersbestoption <139047026+developersbestoption@users.noreply.github.com>
2026-08-09 23:12:27 +0530

    removing zeros to end without changing non zero positions

--- day9/zero_to_end.py
+++ day9/zero_to_end.py
@@ -1,5 +1,10 @@
 a=list(map(int,input("enter array elements :").split()))
-print(a)
-a.sort(reverse=True)
-print(a)
-'''moving zeros to the end'''
+new=[]
+new2=[]
+for i in a:
+    if i != 0:
+        new.append(i)
+    else:
+        new2.append(i)
+new.extend(new2)
+print(new)
