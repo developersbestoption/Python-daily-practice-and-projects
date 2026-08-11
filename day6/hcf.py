c435dde43d3344334aa1d60947ed936c7034e449
developersbestoption <139047026+developersbestoption@users.noreply.github.com>
2026-08-11 23:11:24 +0530

    finding hcf

--- day6/hcf.py
+++ day6/hcf.py
@@ -2,5 +2,5 @@ a=int(input("enter the first number :"))
 b=int(input("enter the second number : "))
 for i in range(min(a,b),1,-1):
     if a%i==0 and b%i==0:
-        print(i,"is the hcf ")
+        print(i,"is the hcf ") 
         break
